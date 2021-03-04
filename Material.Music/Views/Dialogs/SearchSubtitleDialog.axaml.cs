274ec3815370560d7422c50e20a9b10062b51f32
AppleNeko2001 <appleneko2001@outlook.com>
2021-03-05 03:52:02 +0900

    Initial search subtitle feature

--- Material.Music/Views/Dialogs/SearchSubtitleDialog.axaml.cs
+++ Material.Music/Views/Dialogs/SearchSubtitleDialog.axaml.cs
@@ -1,19 +1,76 @@
-﻿using Avalonia;
+﻿using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using Material.Dialog;
+using Material.Dialog.Interfaces;
+using Material.Music.Core;
+using Material.Music.Core.Containers;
+using Material.Music.Views.Dialogs.ViewModels;
 
 namespace Material.Music.Views.Dialogs
 {
     public class SearchSubtitleDialog : UserControl
     {
+        private SearchSubtitleViewModel _viewModel;
+        private PlayableBase _targetPlayable;
+        private IDialogWindow<DialogResult> _dialogHost;
+        
         public SearchSubtitleDialog()
         {
             this.InitializeComponent();
+
+            DataContext = _viewModel = new SearchSubtitleViewModel();
         }
 
+        public void SetHost(IDialogWindow<DialogResult> dialog) => _dialogHost = dialog;
+        
+        public void SetTarget(PlayableBase target)
+        {
+            _targetPlayable = target;
+            _viewModel.Keywords = $"{_targetPlayable.TrackInfo.Artist} {_targetPlayable.Title}";
+        }
+
+        public SearchSubtitleResultItemViewModel GetResult() => _viewModel.SelectedItem;
+        
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void Button_OnClick(object? sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var item = button.DataContext as SearchSubtitleResultItemViewModel;
+            _viewModel.SelectedItem = item;
+            _viewModel.CarouselIndex = 1;
+        }
+
+        private void BackButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+            _viewModel.SelectedItem = null;
+            _viewModel.CarouselIndex = 0;
+        }
+
+        private void DownloadButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    _viewModel.Downloading = true;
+                    var result = _viewModel.selectedProvider.DownloadSubtitle(_viewModel.SelectedItem.Id);
+                    DataContainer.SaveSubtitle(result, _targetPlayable);
+                    Dispatcher.UIThread.InvokeAsync(() => _dialogHost?.GetWindow().Close());
+                }
+                catch
+                {
+                    
+                }
+                _viewModel.Downloading = false;
+            });
         }
     }
 }
--- Material.Music/Views/Dialogs/ViewModels/SearchSubtitleViewModel.cs
+++ Material.Music/Views/Dialogs/ViewModels/SearchSubtitleViewModel.cs
@@ -1,9 +1,127 @@
-﻿using Material.Music.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Threading;
+using JetBrains.Annotations;
+using Material.Music.Core;
+using Material.Music.Online;
+using Material.Music.Online.Interfaces;
+using Material.Music.Online.Results;
+using Material.Music.ViewModels;
 
 namespace Material.Music.Views.Dialogs.ViewModels
 {
+    public class SearchSubtitleResultItemViewModel : ViewModelBase
+    {
+        private SearchSubtitleResultItem _base;
+        public SearchSubtitleResultItemViewModel(SearchSubtitleResultItem data)
+        {
+            _base = data;
+        }
+
+        public long Id => _base.TrackInfo.Id;
+        
+        public string Name => _base.Name;
+
+        public string Title => _base.TrackInfo?.Title;
+
+        public string Album => _base.TrackInfo?.Album;
+
+        public string Artists => _base.TrackInfo?.Artists;
+    }
+    
     public class SearchSubtitleViewModel : ViewModelBase
     {
+        public ISubtitleProvider selectedProvider;
+
+        public SearchSubtitleViewModel()
+        {
+            selectedProvider = ApiManager.Instance.SubtitleProviders.First();
+        }
+        
+        private bool _searchProcessing;
+        public bool SearchProcessing
+        {
+            get => _searchProcessing;
+            set
+            {
+                _searchProcessing = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private bool _downloading;
+        public bool Downloading
+        {
+            get => _downloading;
+            set
+            {
+                _downloading = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void Search()
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    SearchProcessing = true;
+
+                    var result = selectedProvider.SearchSubtitle(_keywords);
+                    ResultList.Clear();
+                
+                    foreach (var item in result.ResultItems)
+                    {
+                        Dispatcher.UIThread.InvokeAsync(()=> ResultList.Add(new SearchSubtitleResultItemViewModel(item)));
+                    }
+                }
+                catch(Exception e)
+                {
+                    SelectedItemTip = e.Message;
+                }
+                SearchProcessing = false;
+            });
+        }
+
+        private string _keywords;
+        public string Keywords
+        {
+            get => _keywords;
+            set
+            {
+                _keywords = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _carouselIndex;
+        public int CarouselIndex
+        {
+            get => _carouselIndex;
+            set
+            {
+                _carouselIndex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private SearchSubtitleResultItemViewModel _selectedItem;
+        public SearchSubtitleResultItemViewModel SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<SearchSubtitleResultItemViewModel> _resultList = new ObservableCollection<SearchSubtitleResultItemViewModel>();
+        public ObservableCollection<SearchSubtitleResultItemViewModel> ResultList => _resultList; 
+
         private string _selectedItemTip;
 
         public string SelectedItemTip
