97bd358242b4444b0decbb0a9d9a9a65ede014c7
AppleNeko2001 <appleneko2001@outlook.com>
2021-03-05 03:52:20 +0900

    Update subtitle viewer page

--- Material.Music/ViewModels/SubtitlePageViewModel.cs
+++ Material.Music/ViewModels/SubtitlePageViewModel.cs
@@ -1,9 +1,27 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using LyricXml;
+using Material.Music.Core;
+using Material.Music.Core.Containers;
+using Opportunity.LrcParser;
 
 namespace Material.Music.ViewModels
 {
     public class SubtitlePageItemViewModel : ViewModelBase
     {
+        private TimeSpan _timestamp;
+
+        public TimeSpan Timestamp
+        {
+            get => _timestamp;
+            set
+            {
+                _timestamp = value;
+                OnPropertyChanged();
+            }
+        }
+
         private string _firstLine;
 
         public string FirstLine
@@ -34,5 +52,54 @@ namespace Material.Music.ViewModels
         private ObservableCollection<SubtitlePageItemViewModel> _subtitleItems;
 
         public ObservableCollection<SubtitlePageItemViewModel> SubtitleItems => _subtitleItems;
+
+        public SubtitlePageViewModel()
+        {
+            _subtitleItems = new ObservableCollection<SubtitlePageItemViewModel>();
+        }
+
+        public void UpdateSubtitlePage(PlayableBase playable)
+        {
+            if (playable is null)
+            {
+                _subtitleItems.Clear();
+                return;
+            }
+
+            try
+            {
+                var lyric = DataContainer.LoadSubtitle(playable);
+                if (lyric != null)
+                {
+                    if (lyric.Timelines == null)
+                        return;
+                
+                    string GetLine(LyricTimeline t, int i)
+                    {
+                        if (lyric.Languages.Length < i)
+                            return null;
+                        
+                        var k = lyric.Languages[i];
+                        if(t.Elements.ContainsKey(k)) 
+                            return t.Elements[k];
+                        return null;
+                    }
+                
+                    foreach (var timestamp in lyric.Timelines)
+                    {
+                        SubtitleItems.Add(new SubtitlePageItemViewModel()
+                        {
+                            FirstLine = GetLine(timestamp, 0),
+                            SecondLine = GetLine(timestamp, 1),
+                            Timestamp = timestamp.Timestamp
+                        });
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
     }
 }
--- Material.Music/Views/Pages/SubtitlePage.axaml.cs
+++ Material.Music/Views/Pages/SubtitlePage.axaml.cs
@@ -1,33 +1,103 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 using Material.Dialog;
 using Material.Dialog.Enums;
 using Material.Icons;
 using Material.Icons.Avalonia;
+using Material.Music.Core.Containers;
+using Material.Music.Core.Engine;
 using Material.Music.ViewModels;
 using Material.Music.Views.Dialogs;
 using Material.Music.Views.Interfaces;
 using Material.Music.Models;
+using Material.Music.Online;
 
 namespace Material.Music.Views.Pages
 {
     public class SubtitlePage : UserControl, IHasActionBarMenus
     {
+        private SubtitlePageViewModel _viewModel;
         private Control[] _actionBarMenus;
+        private ItemsControl SubtitleTextContainer;
+        private ScrollViewer SubtitleScroller;
+        private SubtitlePageItemViewModel _previousItem;
         
         public SubtitlePage()
         {
             this.InitializeComponent();
 
+            SubtitleTextContainer = this.Get<ItemsControl>(nameof(SubtitleTextContainer));
+            SubtitleScroller = this.Get<ScrollViewer>(nameof(SubtitleScroller));
+            
             _actionBarMenus = new[]
             {
                 CreateActionMenu(MaterialIconKind.Magnify, SearchSubtitle, "Search subtitle / lyric"),
                 CreateActionMenu(MaterialIconKind.FileUploadOutline, null, "Load local subtitle / lyric")
             };
+
+            var engine = BassEngine.GetInstance();
+            DataContext = _viewModel = new SubtitlePageViewModel();
+            engine.OnUpdateTick += BackendEngineOnUpdateTick;
             
-            DataContext = PlayerContext.GetInstance();
+            engine.OnChannelStatusChanged += (sender, args) => { _viewModel.UpdateSubtitlePage(args.Playable); };
+            DataContainer.OnSavedSubtitle += (sender, s) =>
+            {
+                var context = PlayerContext.GetInstance();
+                if(s == context.CurrentMedia.GetObjectHash())
+                    _viewModel.UpdateSubtitlePage(context.CurrentMedia);
+            };
+        }
+
+        private async void BackendEngineOnUpdateTick(object? sender, double e)
+        {
+            if (SubtitleTextContainer.ItemCount == 0)
+                return;
+
+            if (GetRequirementsBringViewInto(e, out var index))
+            {
+                var item = await GetSubtitleItem(index);
+                if (item != null)
+                {
+                    await Dispatcher.UIThread.InvokeAsync(() =>
+                    {
+                        item.BringIntoView(new Rect(SubtitleScroller.Bounds.X, SubtitleScroller.Bounds.Y, SubtitleScroller.Bounds.Width, SubtitleScroller.Bounds.Height / 2));
+                    });
+                }
+            }
+        }
+        
+        private bool GetRequirementsBringViewInto(double t, out int index)
+        {
+            //var ts = TimeSpan.FromSeconds(t);
+
+            index = 0;
+            foreach (SubtitlePageItemViewModel item in SubtitleTextContainer.Items)
+            {
+                if (t > item.Timestamp.TotalSeconds)
+                {
+                    index++;
+                    continue;
+                }
+                else
+                {
+                    if(_previousItem == item)
+                        return false;
+                    _previousItem = item;
+                }
+            }
+            return true;
+        }
+
+        private async Task<IControl> GetSubtitleItem(int index)
+        {
+            return await Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                return SubtitleTextContainer.ItemContainerGenerator.ContainerFromIndex(index);
+            });
         }
 
         private void InitializeComponent()
@@ -39,15 +109,23 @@ namespace Material.Music.Views.Pages
 
         public async void SearchSubtitle()
         {
-            /*var dialog = DialogHelper.CreateCustomDialog(new CustomDialogBuilderParams()
+            var context = PlayerContext.GetInstance();
+            var content = new SearchSubtitleDialog();
+
+            if (context.CurrentMedia is null)
+                return;
+            
+            content.SetTarget(context.CurrentMedia);
+            var dialog = DialogHelper.CreateCustomDialog(new CustomDialogBuilderParams()
             {
                 Borderless = true,
-                DialogButtons = DialogHelper.CreateSimpleDialogButtons(DialogButtonsEnum.OkCancel),
+                DialogButtons = DialogHelper.CreateSimpleDialogButtons(DialogButtonsEnum.Ok),
                 ContentHeader = "Search subtitle of track",
-                Width = 800,
-                Content = new SearchSubtitleDialog()
+                Content = content,
+                Width = 600,
             });
-            await dialog.ShowDialog(MainWindow.Instance);*/
+            content.SetHost(dialog);
+            var result = await dialog.ShowDialog(MainWindow.Instance);
         }
 
         public static Control CreateActionMenu(MaterialIconKind iconKind, Action onClick, string tip)
