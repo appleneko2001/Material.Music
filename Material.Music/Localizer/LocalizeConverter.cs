05ac34ba9529b3bcf06d9c2465283d608616ba3d
AppleNeko2001 <appleneko2001@outlook.com>
2021-03-05 18:00:16 +0900

    Add localize converter

---
+++ Material.Music/Localizer/LocalizeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Material.Music.Localizer
+{
+    public class LocalizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var keyToUse = value as string;
+            
+            var context = parameter as string;
+            if (!string.IsNullOrWhiteSpace(context))
+                keyToUse = $"{context}.{value}";
+
+            return Localizer.Instance[keyToUse];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
